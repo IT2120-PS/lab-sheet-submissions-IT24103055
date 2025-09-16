d4bccaa509ffea8f79856872abb23e4639d73225
IT24103055 <IT24103055@my.sliit.lk>
2025-09-16 18:43:27 +0530

    Lab 7

---
+++ Lab Sheet 07/IT24103055.R
@@ -0,0 +1,9 @@
+setwd("C:\\Users\\kavindu\\OneDrive\\Desktop\\IT24103055")
+#1
+punif(25, min = 0, max = 40 ,lower.tail=TRUE) - punif(10, min = 0, max = 40, lower.tail=TRUE)
+#2
+pexp(2, rate = 1/3,lower.tail = TRUE)
+#3(i)
+1 - pnorm(130, mean = 100, sd = 15,lower.tail=TRUE)
+#(ii)
+qnorm(0.95, mean = 100, sd = 15,lower.tail=TRUE)
